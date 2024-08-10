e983e84d3001e722a3e54c744834cbf82c2d7400
Mohammad Azmee Tofail <iazmee101@gmail.com>
2024-08-11 00:22:53 +0600

    duplicates

---
+++ Array/Contains-Duplicates.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+#define NEED_FOR_SPEED ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+bool containsDuplicate(vector<int>& v){
+
+    unordered_set<int> s;
+
+    for(int i = 0; i < v.size(); i++){
+
+        s.insert(v[i]);
+    }
+
+    if(s.size() == v.size()) return false;
+    return true;
+}
+
+void solve(){
+    int n; cin >> n;
+    vector<int> v(n);
+
+    for(int i = 0; i < n; i++){
+
+        cin >> v[i];
+    }
+
+    cout << containsDuplicate(v) << '\n';
+    return;
+}
+
+int32_t main()
+{
+    NEED_FOR_SPEED;
+    int t = 1; 
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
